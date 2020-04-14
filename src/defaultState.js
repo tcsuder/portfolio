82ab29bf3cb7f8ba19a2c9b68263990125375ee1
tcsuder@gmail.com <tcsuder@gmail.com>
2020-04-14 15:34:20 -0700

    remove picture

--- src/defaultState.js
+++ src/defaultState.js
@@ -1,22 +1,18 @@
-import trail3 from './images/trail3.jpg';
 import trail10 from './images/trail10.jpg';
 import trail20 from './images/trail20.jpg';
 import trail22 from './images/trail22.jpg';
 import trail23 from './images/trail23.jpg';
-import trail3mobile from './images/trail3mobile.jpg';
 import trail22mobile from './images/trail22mobile.jpg';
 import trail23mobile from './images/trail23mobile.jpg';
 
 export default {
   largeImageList: {
-    trail3,
     trail10,
     trail20,
     trail22,
     trail23
   },
   smallImageList: {
-    trail3mobile,
     trail22mobile,
     trail23mobile,
   },
